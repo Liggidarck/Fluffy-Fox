e49289992009ef2b3fddb9a290002e3a23a789c5
George Filatov <georgyfilatov@yandex.ru>
2023-06-25 13:17:30 +0300

    init kotlin voltage offline

---
+++ kotlin_voltage_offline/src/main/java/com/george/android/kotlin_voltage_offline/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.george.android.kotlin_voltage_offline.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
